116df63bd6456f7ce62388f9e96f6e1ad74ee241
Ari Davis <ari.davis@gdn-commerce.com>
2021-11-20 20:51:14 +0700

    build

--- src/constants/ApiPath.js
+++ src/constants/ApiPath.js
@@ -3,4 +3,7 @@ import { Constant } from "./Constant";
 export const ApiPath = {
   GET_CP_QUESTION_DETAIL: Constant.API_PATH + "/codes/{}",
   SUBMIT_CP_QUESTION: Constant.API_PATH + "/codes/submit",
+
+  LOGIN: Constant.API_PATH + "/auth/login",
+  LOGOUT: Constant.API_PATH + "/auth/login",
 };
