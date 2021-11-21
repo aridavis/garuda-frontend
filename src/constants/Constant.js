3e657a312d396e4af5036990ec968ac022a812ae
Ari Davis <ari.davis@gdn-commerce.com>
2021-11-21 21:19:48 +0700

    fix

--- src/constants/Constant.js
+++ src/constants/Constant.js
@@ -1,7 +1,6 @@
 export const Constant = {
-  API_PATH: "http://localhost:8000/api",
-  BACKEND_URL: "http://localhost:8000",
-  // API_PATH: "https://backend.occupedia.co/api",
+  BACKEND_URL: "https://backend.occupedia.co/api",
+  API_PATH: "https://backend.occupedia.co/api",
   SOCKET_URL: "http://localhost:5000",
   ACCESS_TOKEN: "ACCESS_TOKEN",
 };
